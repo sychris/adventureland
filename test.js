22abb853c8a1206f8ce13e295d3224cf0466585f
sychris <sychris9@gmail.com>
2021-04-18 01:54:45 -0700

    cleanup

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 setInterval(test_fun, 250)
 
 function test_fun(){
-	log(JSON.stringify(character))
+	log(JSON.stringify(character.items))
 }
-log(JSON.stringify(character))
+log(JSON.stringify(character.items))
