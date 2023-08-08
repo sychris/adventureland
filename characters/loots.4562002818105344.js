52e1b5461aab99214303e1550d3d4d5eff2f5e52
sychris <sychris9@gmail.com>
2023-08-08 09:24:08 -0400

    Update loots.4562002818105344.js
    no longer relying on merc.2.js

--- characters/loots.4562002818105344.js
+++ characters/loots.4562002818105344.js
@@ -1,10 +1,84 @@
-// Node standard library modules to access home directory and convert path to file:/// URI.
-const os = require("os");
-const url = require("url");
+//Merc
+log("Starting merc script")
+load_code(10)
+//test area
+//send_item("Sychris", getItemSlot("mpot0"), 2)
 
-// Grab your documents folder with your custom CODE directory inside of it.
-//const homedir = url.pathToFileURL(os.homedir());
-const custom_code_dir = "D:\coding_projects\adventureland\codes";
+//this shows trade history
+//show_json(parent.socket.emit('trade_history'))
 
-// Load the main file from your ES6 module.
-import(custom_code_dir + "\merc.2.js");
+//this shows crafting chance and grace value
+//parent.upgrade(26, locate_item("scroll0"), null, true, true).then(e=>show_json(e));
+
+//todo:
+//convert configs.upgrade.upgradeWhitelist to a map
+//convert configs.upgrade.combineWhitelist to a map
+// convert pots_to_buy to a map
+//auto open and close stall based on movement
+
+
+
+
+
+configs.buyPots.enabled = true
+configs.buyPots.pots_to_buy = {
+  //ItemName, Count
+  mpot0: 8000,
+  mpot1: 8000,
+  hpot0: 5000
+}
+
+//currently exchanges item in slot 0 with nearby npc
+configs.exchangeItems.enabled = false
+configs.give_pots.enabled = true
+configs.upgrade.enable = true
+configs.luck.enabled = true
+configs.sell.enabled = true
+configs.travelToPlayers.enabled = false
+configs.autoStand.enabled = true
+
+configs.travelToPlayers.targetPlayerName = "Sychris"
+
+configs.autocraft.enabled = false
+configs.autocraft.item = "cake"
+
+configs.upgradeNPCItem.item = "staff"
+configs.upgradeNPCItem.enabled = true
+
+configs.buyPonty.enabled = true
+
+configs.buyMercs.enabled = true
+configs.buyMercs.maxToSpend = 1000000
+
+configs.regen.to_percent = 80
+
+configs.sell.items.set('test_orb', 0)
+configs.sell.items.set('hpamulet', 0)
+configs.sell.items.set('wand', 0)
+configs.sell.items.set('stinger', 0)
+configs.sell.items.set('hpbelt', 0)
+configs.sell.items.set('cclaw', 0)
+
+configs.buyPonty.itemsList = [
+  "dexamulet",
+  "stramulet",
+  "intamulet",
+  "intring",
+  "intearring",
+  "dexring",
+  "dexearring"
+]
+
+var regen_mp_Interval = 1000
+
+//this is where all the intervals etc are
+load_code(40)
+
+
+
+
+
+
+
+
+
