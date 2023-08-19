03c6ac3111ad5157a8ff07f8dcd86de79693fa95
sychris <sychris9@gmail.com>
2023-08-19 00:12:34 -0400

    cleanup of request_pots

--- codes/util_combat.21.js
+++ codes/util_combat.21.js
@@ -15,28 +15,21 @@ function inv_dump() {
 }
 
 function request_pots(name) {
-  let requests = {}
   for (const potion in configs.pots.pots_to_request) {
+    const needed = {
+      type: "pots",
+    };
   
-    let needed = {}
-    needed.type = "pots"
+    const currentSlot = getItemSlot(potion);
+    const currentCount = currentSlot === -1 ? 0 : getItemQuantity(potion);
   
-    if (getItemSlot(potion) === -1) {
-      needed.name = potion
-      needed.q = configs.pots.pots_to_request[potion]
+    if (currentSlot === -1 || currentCount < configs.pots.pots_to_request[potion]) {
+      needed.name = potion;
+      needed.q = configs.pots.pots_to_request[potion] - currentCount;
     
-    } else {
-      let currentCount = getItemQuantity(potion)
-      if (currentCount < configs.pots.pots_to_request[potion]) {
-        needed.name = potion
-        needed.q = configs.pots.pots_to_request[potion] - currentCount
-        writeToLog("looks like we need " + JSON.stringify(needed))
-      }
-    }
+      writeToLog("Looks like we need " + needed.q + " " + needed.name);
     
-    if (needed.name) {
-      writeToLog("requesting " + needed.pot + needed.keys)
-      send_cm(name, needed)
+      send_cm(name, needed);
     }
   }
 }
@@ -74,7 +67,7 @@ function getNearestMonster(args) {
   // no_target: Only pick monsters that don't have any target
   // path_check: Checks if the character can move to the target
   // type: Type of the monsters, for example "goo", can be referenced from `show_json(G.monsters)` [08/02/17]
-  args.path_check = undefined;
+  
   let min_d = 999, target = null;
   
   if (!args) args = {
@@ -83,29 +76,32 @@ function getNearestMonster(args) {
     no_target: false,
   };
   if (args && args.target && args.target.name) args.target = args.target.name;
-  if (args && args.type == "monster") writeToLog("get_nearest_monster: you used monster.type, which is always 'monster', use monster.mtype instead");
+  if (args && args.type === "monster") writeToLog("get_nearest_monster: you used monster.type, which is always 'monster', use monster.mtype instead");
   if (args && args.mtype) writeToLog("get_nearest_monster: you used 'mtype', you should use 'type'");
   
   for (let id in parent.entities) {
     let current = parent.entities[id];
-    
-    if (current.type != "monster" || !current.visible || current.dead) continue;
+  
+    if (current.type !== "monster" || !current.visible || current.dead) continue;
     if (args.min_xp && current.xp < args.min_xp) continue;
     if (args.max_att && current.attack > args.max_att) continue;
-    if (args.target && current.target != args.target) continue;
-    if (args.no_target && current.target && current.target != character.name) continue;
+    if (args.target && current.target !== args.target) continue;
+    if (args.no_target && current.target && current.target !== character.name) continue;
     if (args.path_check && !can_move_to(current)) continue;
-    
+  
     if (args.type) {
       if (Array.isArray(args.type)) {
         if (!args.type.includes(current.mtype)) continue;
       } else if (typeof args.type === 'string' || args.type instanceof String) {
-        if (current.mtype != args.type) continue;
+        if (current.mtype !== args.type) continue;
       }
     }
-    
+  
     let c_dist = parent.distance(character, current);
-    if (c_dist < min_d) min_d = c_dist, target = current;
+    if (c_dist < min_d) {
+      min_d = c_dist
+      target = current
+    }
   }
   return target;
 }
