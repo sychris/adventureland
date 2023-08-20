92c1e8805af0bb77600170e377f768d917a52f34
sychris <sychris9@gmail.com>
2023-08-20 14:53:26 -0400

    added a parent.character for testing with

---
+++ test/test.parent.character.js
@@ -0,0 +1,320 @@
+character = {
+  "x": 721,
+  "y": 455,
+  "width": 52,
+  "height": 68,
+  "alpha": 1,
+  "visible": true,
+  "cskin": "10",
+  "i": 1,
+  "j": 0,
+  "skin": "mranger2",
+  "stype": "full",
+  "updates": 533838,
+  "cscale": 2,
+  "in": "main",
+  "map": "main",
+  "hp": 5965,
+  "max_hp": 6063,
+  "mp": 940,
+  "max_mp": 1240,
+  "xp": 209913533,
+  "attack": 1061,
+  "heal": 0,
+  "frequency": 1.2353253340448462,
+  "speed": 69,
+  "range": 234,
+  "armor": 209,
+  "resistance": 281,
+  "level": 72,
+  "rip": false,
+  "afk": false,
+  "s": {
+    "mluck": {
+      "ms": 2220814,
+      "f": "loots",
+      "strong": true
+    }
+  },
+  "c": {},
+  "q": {},
+  "age": 1452,
+  "pdps": 275132.5279515176,
+  "id": "mrshoots",
+  "cid": 341914,
+  "stand": false,
+  "cx": {},
+  "slots": {
+    "ring1": {
+      "name": "ringsj",
+      "level": 4
+    },
+    "ring2": {
+      "level": 3,
+      "name": "dexring"
+    },
+    "earring1": {
+      "name": "dexearring",
+      "level": 2
+    },
+    "earring2": {
+      "name": "dexearring",
+      "level": 3
+    },
+    "belt": {
+      "level": 3,
+      "name": "hpbelt"
+    },
+    "mainhand": {
+      "name": "hbow",
+      "level": 9
+    },
+    "offhand": {
+      "level": 8,
+      "name": "quiver"
+    },
+    "helmet": {
+      "stat_type": "dex",
+      "name": "wcap",
+      "level": 8
+    },
+    "chest": {
+      "name": "tshirt1",
+      "level": 7
+    },
+    "pants": {
+      "acc": 91,
+      "name": "frankypants",
+      "level": 6,
+      "ach": "gooped",
+      "stat_type": "dex"
+    },
+    "shoes": {
+      "name": "vboots",
+      "level": 5,
+      "stat_type": "dex"
+    },
+    "gloves": {
+      "stat_type": "dex",
+      "name": "wgloves",
+      "level": 7
+    },
+    "amulet": {
+      "name": "dexamulet",
+      "level": 3
+    },
+    "orb": {
+      "name": "orbofdex",
+      "level": 3
+    },
+    "elixir": null,
+    "cape": {
+      "acc": 35548,
+      "ps": [
+        "festive"
+      ],
+      "name": "gcape",
+      "level": 6,
+      "ach": "festive",
+      "p": "festive",
+      "stat_type": "dex"
+    }
+  },
+  "ctype": "ranger",
+  "owner": "5168369757061120",
+  "int": 46,
+  "str": 37,
+  "dex": 266,
+  "vit": 48,
+  "for": 8,
+  "mp_cost": 23,
+  "mp_reduction": 0,
+  "max_xp": 420000000,
+  "goldm": 1.055,
+  "xpm": 1.17,
+  "luckm": 1.13,
+  "isize": 42,
+  "esize": 36,
+  "gold": 4152,
+  "cash": 0,
+  "targets": 0,
+  "m": 0,
+  "evasion": 1,
+  "miss": 0,
+  "reflection": 1,
+  "lifesteal": 0,
+  "manasteal": 0,
+  "rpiercing": 3,
+  "apiercing": 96,
+  "crit": 0,
+  "critdamage": 0,
+  "dreturn": 1,
+  "tax": 0.02,
+  "xrange": 25,
+  "pnresistance": 21,
+  "firesistance": 8,
+  "fzresistance": 0,
+  "phresistance": 0,
+  "stresistance": 0,
+  "incdmgamp": 0,
+  "stun": 0,
+  "blast": 0,
+  "explosion": 0,
+  "courage": 2,
+  "mcourage": 2,
+  "pcourage": 2,
+  "fear": 0,
+  "items": [
+    {
+      "q": 7997,
+      "name": "mpot0"
+    },
+    {
+      "q": 8000,
+      "name": "hpot0"
+    },
+    {
+      "q": 8000,
+      "name": "mpot1"
+    },
+    {
+      "q": 500,
+      "name": "hpot1"
+    },
+    {
+      "name": "crabclaw",
+      "q": 1
+    },
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    {
+      "name": "tracker"
+    }
+  ],
+  "cc": 3.6,
+  "ipass": "Koue8qJgW4Mf",
+  "home": "USII",
+  "friends": [],
+  "acx": {},
+  "xcx": [
+    "makeup105",
+    "makeup117",
+    "mmakeup00",
+    "fmakeup01",
+    "fmakeup02",
+    "fmakeup03",
+    "marmor5a",
+    "hairdo106",
+    "mbody2b",
+    "hairdo206",
+    "facemakeup02"
+  ],
+  "emx": {},
+  "pzazz": 0.25,
+  "last_ms": "2023-08-20T08:01:00.823Z",
+  "name": "mrshoots",
+  "walking": null,
+  "fx": {
+    "aaa": null,
+    "attack": null
+  },
+  "emblems": {},
+  "real_alpha": 1,
+  "real_x": -1228.1275846944318,
+  "real_y": -134.80413196640345,
+  "type": "character",
+  "me": 1,
+  "base": {
+    "h": 8,
+    "v": 7,
+    "vn": 2
+  },
+  "awidth": 26,
+  "aheight": 34,
+  "explanation": "Hey Adventurer! This is your very own character that is a PIXI.Sprite Object. Adventure Land uses PIXI to draw things. You can learn more about it: https://www.pixijs.com/ and draw your own stuff via CODE! (Technical) Since your character is a special object, it doesn't get drawn like other entities, therefore your real character object has static x and y values. The character object in Code is an Object that mimics and extends your actual character Object. It's kinda complicated, but the only difference is that your real character object has different x and y values.",
+  "ping": 53.099999999999994,
+  "vision": [
+    700,
+    500
+  ],
+  "ntag_cache": "mrshoots|undefined|false|false|false|false|false|false|true",
+  "name_tag": {
+    "x": 0,
+    "y": 0,
+    "width": 1,
+    "height": 1,
+    "alpha": 1,
+    "visible": true,
+    "zy": -999999999999999,
+    "name": null
+  },
+  "last_fear": 0,
+  "listeners": [
+    {
+      "id": "vnXGiVUVhmsvLAKUs3XVHmVeyuEMt6",
+      "event": "stacked"
+    },
+    {
+      "id": "Xv6LhFxi8mZFmvPBCtFfDNg66Q0Pq4",
+      "event": "death"
+    },
+    {
+      "id": "l6b19C8WWVfdcI9oFFTP3yc76UqvMs",
+      "event": "cm"
+    }
+  ],
+  "stats": {
+    "str": 37,
+    "dex": 266,
+    "int": 46,
+    "vit": 48,
+    "for": 8
+  },
+  "code": true,
+  "a_angle": 70.5337981696508,
+  "angle": 70.5337981696508,
+  "a_direction": 0,
+  "direction": 0,
+  "target": "20905185",
+  "focus": null,
+  "ms_walk": "2023-08-20T08:01:00.190Z",
+  "last_stop": "2023-08-20T08:01:00.241Z",
+  "last_walking": 37036,
+  "party": "Sychris"
+}
+
+module.exports = character;
