25b61082c2edaf9f043b3d36dfe98aecb9144255
sychris <sychris9@gmail.com>
2023-08-21 18:47:28 -0400

    moved checkHpMp interval to everyone

--- codes/post_init_combat.41.js
+++ codes/post_init_combat.41.js
@@ -1,6 +1,6 @@
 //post init/intervals
 setInterval(inv_dump, configs.inv_dump.interval)
-setInterval(checkHpMp, configs.hpMp.interval); // Loops every 1/10 seconds.
+
 if (character.ctype === "mage") {
   setInterval(combat, configs.attack.interval)
   setInterval(energize, configs.skills.energize.interval)
@@ -10,5 +10,8 @@ if (character.ctype === "ranger") {
   setInterval(combat, configs.attack.interval);
 }
 
+if (character.ctype === "warrior") {
+  setInterval(combat, configs.attack.interval);
+}
 
 map_key("O", "snippet", "toggle_mode(configs.inv_dump)");
