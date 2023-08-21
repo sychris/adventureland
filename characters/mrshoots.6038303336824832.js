0b3e5ed863f9d2bddea3aba41389574199639a1c
sychris <sychris9@gmail.com>
2023-08-21 05:47:45 -0400

    nulled attack only also letting use pots hp/mp to default

--- characters/mrshoots.6038303336824832.js
+++ characters/mrshoots.6038303336824832.js
@@ -1,24 +1,11 @@
 load_code(10)
 
 configs.hpMp.enabled = true
-configs.hpMp.useMpot1.enabled = true
-configs.hpMp.useMpot1.percent = 20
-configs.hpMp.useMpot0.enabled = true
-configs.hpMp.useMpot0.percent = 50
-configs.hpMp.mpMeditate.Enabled = true
-configs.hpMp.mpMeditate.percent = 90
-
-configs.hpMp.useHpot1.enabled = true
-configs.hpMp.useHpot1.percent = 20
-configs.hpMp.useHpot0.enabled = true
-configs.hpMp.useHpot0.percent = 50
-configs.hpMp.hpMeditate.Enabled = true
-configs.hpMp.hpMeditate.percent = 90
 
 configs.attack.enabled = 1
 configs.attack.allowMoveForAttack = false
-configs.attack.onlyAttack = "crab"
-configs.attack.preferedTarget = "squigtoad"
+configs.attack.onlyAttack = null//"crab"
+configs.attack.preferedTarget = null
 
 configs.sell.enabled = true
 configs.sell.items.set('stinger', 0)
@@ -39,12 +26,7 @@ configs.pots.pots_to_request = {
 }
 
 
-
-
-
-
 var minMpPercentForThreeShot = 25
-
 
 
 load_code(40)
