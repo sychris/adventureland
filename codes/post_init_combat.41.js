d51822e70ce50ddaa46a2605a44b036ccd36de18
sychris <sychris9@gmail.com>
2023-08-21 06:06:56 -0400

    character.ctype == to ===

--- codes/post_init_combat.41.js
+++ codes/post_init_combat.41.js
@@ -1,12 +1,12 @@
 //post init/intervals
 setInterval(inv_dump, configs.inv_dump.interval)
 setInterval(checkHpMp, configs.hpMp.interval); // Loops every 1/10 seconds.
-if (character.ctype == "mage") {
+if (character.ctype === "mage") {
   setInterval(combat, configs.attack.interval)
   setInterval(energize, configs.skills.energize.interval)
 }
 
-if (character.ctype == "ranger") {
+if (character.ctype === "ranger") {
   setInterval(combat, configs.attack.interval);
 }
 
