1ea5c8c51945c084a79b8ac3bafe4c99c0841a17
sychris <sychris9@gmail.com>
2023-08-21 05:48:15 -0400

    added configs and got MrChops there own character file

---
+++ characters/MrChops.4647888473817088.js
@@ -0,0 +1,16 @@
+//this is not meant to be the script you actually run.
+//instead you copy this to the characters personal file and modify default configs.
+log("Starting script")
+load_code(10)
+
+
+configs.attack.enabled = true
+configs.attack.allowMoveForAttack = true
+configs.attack.onlyAttack = null//"crab"
+configs.attack.preferedTarget = "squigtoad"
+
+configs.sell.enabled = true
+
+
+//this is where all the intervals etc are
+load_code(40)
