199624c990a730cecc689ffaf9ba0a135c89df03
sychris <sychris9@gmail.com>
2023-08-21 06:06:12 -0400

    fixed mage combat interval

--- codes/post_init_combat.41.js
+++ codes/post_init_combat.41.js
@@ -1,13 +1,13 @@
 //post init/intervals
 setInterval(inv_dump, configs.inv_dump.interval)
 setInterval(checkHpMp, configs.hpMp.interval); // Loops every 1/10 seconds.
-if(character.ctype == "mage"){
-  setInterval(combat, 250)
+if (character.ctype == "mage") {
+  setInterval(combat, configs.attack.interval)
   setInterval(energize, configs.skills.energize.interval)
 }
 
-if(character.ctype == "ranger"){
-  setInterval(combat, configs.attack.interval); // Loops every 1/4 seconds.
+if (character.ctype == "ranger") {
+  setInterval(combat, configs.attack.interval);
 }
 
 
