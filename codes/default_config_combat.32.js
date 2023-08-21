b451c99c7fd289c9b53d004c8437cab26a6426f6
sychris <sychris9@gmail.com>
2023-08-21 05:46:17 -0400

    add pots_to_request to default_config_combat

--- codes/default_config_combat.32.js
+++ codes/default_config_combat.32.js
@@ -9,7 +9,15 @@ configs.attack.allowMoveForAttack = false
 
 configs.inv_dump = {};
 configs.inv_dump.fname = "Inventory Dump Mode"
-configs.inv_dump.enabled = false
+configs.inv_dump.enabled = true
 configs.inv_dump.interval = 1000
 configs.inv_dump.sendTo = "loots"
 configs.inv_dump.wList = ["tracker", "mpot0", "hpot0", "mpot1", "hpot1", "mpot2", "hpot2"];
+
+configs.pots.pots_to_request = {
+  //ItemName, Count
+  mpot0: 8000,
+  hpot0: 8000,
+  mpot1: 8000,
+  hpot1: 8000,
+}
